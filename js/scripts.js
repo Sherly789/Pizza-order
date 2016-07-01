31da8ee5d4f75103977754c86c32e1051c31c2cf
Sherly789 <sherlysh789>
2016-07-01 13:31:09 -0700

    add toppings and size object and cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+function Order(cheese, tomato, mushroom, pizzaSize) {
+  this.cheese = cheese;
+  this.tomato = tomato;
+  this.mushroom = mushroom;
+  this.pizzaSize = pizzaSize;
+}
+
+Order.prototype.cost = function() {
+  var cost = 0;
+  if (this.pizzaSize === "Small" ) {
+      cost += 6;
+  } else {
+    alert("1000");
+  }
+  return cost;
+}
+
 // User interface
 $(document).ready(function() {
   $("form#order").submit(function(event) {
@@ -14,8 +31,13 @@ $(document).ready(function() {
     if ($("#mushroom").prop('checked')) {
       mushroom = 1;
     }
-    // var toppingInput = $("input:checkbox[name=topping]:checked").val();
+
     var pizzaSize = $("input:radio[name=size]:checked").val();
+    var orderobject = new Order(cheese, tomato, mushroom, pizzaSize);
+    var output = orderobject.cost();
+
+    console.log(orderobject);
+    $("#output").text(output)
 
 
   });
